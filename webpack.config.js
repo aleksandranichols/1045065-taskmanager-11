37f9ee6a821c7a587ed1fabfe99261ee8c66d232
aleksandranichols <aleksandra.nichols@gmail.com>
2020-04-11 17:46:11 -0500

    Add webpack settings and divide main.js into components

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 3000
+  }
+};
